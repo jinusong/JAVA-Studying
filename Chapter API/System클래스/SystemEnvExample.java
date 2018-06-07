245cad1fb8a4e753c6daa88fc760af1e1562f44f
jinusong <sugwer7464@gmail.com>
2018-06-07 19:45:10 +0900

    환경 변수 읽기(getenv())

---
+++ Chapter API/System클래스/SystemEnvExample.java
@@ -0,0 +1,11 @@
+package System클래스;
+
+//환경 변수 읽기(getenv())
+//JAVA_HOME 환경 변수 값 얻기
+public class SystemEnvExample {
+	public static void main(String [] args) {
+		String javaHome = System.getenv("JAVA_HOME");
+		System.out.println("JAVA_HOME: " + javaHome);
+	}
+}
+ 
