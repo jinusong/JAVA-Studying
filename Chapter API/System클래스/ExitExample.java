ae9c0016fc045f47455bca4c8c25482e79caecda
jinusong <sugwer7464@gmail.com>
2018-06-07 19:35:33 +0900

    프로그램 종료(exit())

--- Chapter API/System클래스/ExitExample.java
+++ Chapter API/System클래스/ExitExample.java
@@ -5,7 +5,7 @@ package System클래스;
 public class ExitExample {
 	public static void main(String[] args) {
 		//보안 관리자 설정
-		System.setSecurityManager(new SecurityManager() {;
+		System.setSecurityManager(new SecurityManager() {
 		@Override
 		public void checkExit(int status) {
 			if(status != 5) {
@@ -22,4 +22,4 @@ public class ExitExample {
 			} catch (SecurityException e) {}
 		}
 	}
-}
+} 
