7a8480373deb1de0b47ee9620db274fc0fb9ee50
jinusong <sugwer7464@gmail.com>
2018-06-08 23:46:25 +0900

    문자열 길이(length())

---
+++ Chapter API/String클래스/StringLengthExample.java
@@ -0,0 +1,15 @@
+package String클래스;
+
+//문자열 길이(length())
+//문자열의 문자 수 얻기
+public class StringLengthExample {
+	public static void main(String[] args) {
+		String ssn = "7306241230123";
+		int length = ssn.length();
+		if(length == 13) {
+			System.out.println("주민번호 자리수가 맞습니다.");
+		} else {
+			System.out.println("주민번호 자리수가 틀립니다.");
+		}
+	}
+}
