e92b54379c1c82b5964e571946c903da9bec9879
jinusong <sugwer7464@gmail.com>
2018-06-08 23:48:15 +0900

    Pattern 클래스

---
+++ Chapter API/정규표현식과Pattem클래스/PatternExample.java
@@ -0,0 +1,27 @@
+package 정규표현식과Pattem클래스;
+
+//Pattern 클래스
+//문자열 검증하기
+import java.util.regex.Pattern;
+
+public class PatternExample {
+	public static void main(String[] args) {
+		String regExp = "(02|010)-\\d{3,4}-\\d{4}";
+		String data = "010-123-4567";
+		boolean result = Pattern.matches(regExp, data);
+		if(result) {
+			System.out.println("정규식과 일치합니다.");
+		} else {
+			System.out.println("정규식과 일치하지 않습니다.");
+		}
+		
+		regExp = "\\w+@\\w+\\.\\w+(\\.\\w+)?";
+		data = "angel@naver.com";
+		result = Pattern.matches(regExp, data);
+		if(result) {
+			System.out.println("정규식과 일치합니다.");
+		} else {
+			System.out.println("정규식과 일치하지 않습니다.");
+		}
+	}
+}
