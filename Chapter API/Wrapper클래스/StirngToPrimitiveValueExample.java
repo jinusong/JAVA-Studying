d8824c0674eaf5afed1e3ef0864f8ee7d5b4d24a
jinusong <sugwer7464@gmail.com>
2018-06-08 23:47:49 +0900

    문자열을 기본 타입 값으로 젼환

---
+++ Chapter API/Wrapper클래스/StirngToPrimitiveValueExample.java
@@ -0,0 +1,15 @@
+package Wrapper클래스;
+
+//문자열을 기본 타입 값으로 젼환
+
+public class StirngToPrimitiveValueExample {
+	public static void main(String[] args) {
+		int value1 = Integer.parseInt("10");
+		double value2 = Double.parseDouble("3.14");
+		boolean value3 = Boolean.parseBoolean("true");
+		
+		System.out.println("value1: " + value1);
+		System.out.println("value2: " + value2);
+		System.out.println("value3: " + value3);
+	}
+}
