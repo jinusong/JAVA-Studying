d4d132bddb3d33b4b3ee78f427e47b2a41688793
jinusong <sugwer7464@gmail.com>
2018-06-10 23:27:13 +0900

    제네릭타입(class<T>,interface<T>)

--- Chapter 제네릭/제네릭타입/Box.java
+++ Chapter 제네릭/제네릭타입/Box.java
@@ -5,7 +5,7 @@ package 제네릭타입;
 //Box 클래스
 
 public class Box {
-	private Object object;
+	private Object object; 
 	public void set(Object object) { this.object = object; }
 	public Object get() { return object; }
 }
