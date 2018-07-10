99636a0485b26eb6dc76610f54b3cfe0d7226dbb
jinusong <sugwer7464@gmail.com>
2018-07-10 23:23:59 +0900

    Supplier 함수적 인터페이스

---
+++ Chapter 람다식/표준API의_함수적인터페이스/SupplierExample.java
@@ -0,0 +1,15 @@
+package 표준API의_함수적인터페이스;
+
+import java.util.function.IntSupplier;
+
+public class SupplierExample {
+	public static void main(String[] args) {
+		IntSupplier intSupplier = () -> {
+			int num = (int) (Math.random() * 6) + 1;
+			return num;
+		};
+		
+		int num = intSupplier.getAsInt();
+		System.out.println("눈의 수: " + num);
+	}
+}
