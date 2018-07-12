abab3797075e9ebcf93268658fd7ccc03b32cf36
jinusong <sugwer7464@gmail.com>
2018-07-12 23:59:09 +0900

    PredicateExample 함수적 인터페이스

--- Chapter 람다식/표준API의_함수적인터페이스/PredicateExample.java
+++ Chapter 람다식/표준API의_함수적인터페이스/PredicateExample.java
@@ -1,5 +1,17 @@
 package 표준API의_함수적인터페이스;
 
-public class PredicateExample {
+import java.util.Arrays;
 
+//표준 API의 함수적 인터페이스
+//PredicateExample 함수적 인터페이스
+
+public class PredicateExample {
+	private static List<Student> list = Arrays.asList(
+			new Student("홍길동", "남자", 90),
+			new Student("김순희", "여자", 90),
+			new Student("감자바", "남자", 95),
+			new Student("박한나", "여자", 92)
+	);
+	
+	public static double
 }
