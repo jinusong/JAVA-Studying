7a5854aeedd267afca39a16ef71a003bbc78f8a8
jinusong <sugwer7464@gmail.com>
2018-07-13 23:58:26 +0900

    PredicateExample 함수적 인터페이스

--- Chapter 람다식/표준API의_함수적인터페이스/PredicateExample.java
+++ Chapter 람다식/표준API의_함수적인터페이스/PredicateExample.java
@@ -1,6 +1,8 @@
 package 표준API의_함수적인터페이스;
 
 import java.util.Arrays;
+import java.util.List;
+import java.util.function.Predicate;
 
 //표준 API의 함수적 인터페이스
 //PredicateExample 함수적 인터페이스
@@ -13,5 +15,22 @@ public class PredicateExample {
 			new Student("박한나", "여자", 92)
 	);
 	
-	public static double
+	public static double avg( Predicate<Student> predicate) {
+		int count = 0, sum = 0;
+		for(Student student : list) {
+			if( predicate.test (student) ) {
+				count++;
+				sum += student.getScore();
+			}
+		}
+		return (double) sum / count;
+	}
+	
+	public static void main(String[] args) {
+		double maleAvg = avg( t -> t.getSex().equals("남자") );
+		System.out.println("남자 평균 점수: " + maleAvg);
+		
+		double femaleAvg = avg(t -> t.getSex().equals("여자") );
+		System.out.println("여자 평균 점수: " + femaleAvg);
+	}
 }
