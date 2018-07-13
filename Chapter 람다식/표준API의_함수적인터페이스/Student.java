bb37f1c64d93c874c5ddd883d8aa861848967b3d
jinusong <sugwer7464@gmail.com>
2018-07-13 23:58:50 +0900

    Function 함수적 인터페이스, PredicateExample 함수적 인터페이스

--- Chapter 람다식/표준API의_함수적인터페이스/Student.java
+++ Chapter 람다식/표준API의_함수적인터페이스/Student.java
@@ -19,3 +19,23 @@ public class Student {
 	public int getEnglishScore() { return englishScore; }
 	public int getMathScore() { return mathScore; }
 }
+
+
+// 표준 API의 함수적 인터페이스
+// PredicateExample 함수적 인터페이스
+// Student 클래스
+
+public class Student {
+	private String name;
+	private String sex;
+	private int score;
+	
+	public Student(String name, String sex, int score) {
+		this.name = name;
+		this.sex = sex;
+		this.score = score;
+	}
+	
+	public String getSex() { return sex; }
+	public int getScore( ) { return score; }
+}
