765518e12c61d57922d22ba8739272e925f361ba
jinusong <sugwer7464@gmail.com>
2018-07-16 23:50:52 +0900

    Set Collection

---
+++ Chapter 컬렉션 프레임워크/Set_컬렉션/Set_Collection.java
@@ -0,0 +1,31 @@
+package Set_컬렉션;
+
+// Set 컬렉션
+// Set 컬렉션 기본문법
+
+public class Set_Collection {
+	/*
+	 Set<String> set = ```;
+	 set.add("홍길동");	//객체 추가
+	 set.add("신용권");
+	 set.remove("홍길동");	//객체 삭제
+	 
+	 Set<String> set =  ```;
+	 Iterator<String> iterator = set.iterator();
+	 while(iterator.hasNext()) {		//hasNext == 다음에 가져올 객체 있으면 true 없으면 false
+	 	//String 객체 하나를 가져옴
+	 	String str = iterator.next();		//저장된 객체만큼 루핑
+	 }
+	 
+	 Set<String> set = ```;
+	 for(String str : set) {		//저장된 객체만큼 루핑
+	 }
+	 
+	 while(iterator.hasNext()) {
+	 	String str = iterator.next();
+	 	if(str.equals("홍길동"){
+	 		iterator.remove();		//객체 검색 후 삭제
+	 	}
+	 }
+	 */
+}
