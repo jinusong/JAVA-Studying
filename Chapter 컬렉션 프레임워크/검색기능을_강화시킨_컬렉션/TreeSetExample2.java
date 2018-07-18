918136e93e68d25cf72c74c47e4ba1201fe9445d
jinusong <sugwer7464@gmail.com>
2018-07-18 23:52:11 +0900

    TreeSet - 객체 정렬하기

--- Chapter 컬렉션 프레임워크/검색기능을_강화시킨_컬렉션/TreeSetExample2.java
+++ Chapter 컬렉션 프레임워크/검색기능을_강화시킨_컬렉션/TreeSetExample2.java
@@ -1,5 +1,31 @@
 package 검색기능을_강화시킨_컬렉션;
 
-public class TreeSetExample2 {
+import java.util.NavigableSet;
+import java.util.TreeSet;
 
+// 검색기능을 강화시킨 컬렉션
+// TreeSet
+// 객체 정렬하기
+
+public class TreeSetExample2 {
+	public static void main(String[] args) {
+		TreeSet<Integer> scores = new TreeSet<Integer>();
+		scores.add(new Integer(87));
+		scores.add(new Integer(98));
+		scores.add(new Integer(75));
+		scores.add(new Integer(95));
+		scores.add(new Integer(80));
+		
+		NavigableSet<Integer> descendingSet = scores.descendingSet();
+		for(Integer score : descendingSet) {
+			System.out.print(score + " ");
+		}
+		System.out.println();
+		
+		NavigableSet<Integer> ascendingSet = descendingSet.descendingSet();
+		
+		for(Integer score : ascendingSet) {
+			System.out.print(score + " ");
+		}
+	}
 }
