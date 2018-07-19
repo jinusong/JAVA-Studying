5144b5926dc9ab9f44ecfcdc7abe69054cf7bf9c
jinusong <sugwer7464@gmail.com>
2018-07-20 08:16:40 +0900

    Comparable과 Comparator - Comparable을 구현하지 않은 클래스

--- Chapter 컬렉션 프레임워크/검색기능을_강화시킨_컬렉션/Fruit.java
+++ Chapter 컬렉션 프레임워크/검색기능을_강화시킨_컬렉션/Fruit.java
@@ -1,5 +1,15 @@
 package 검색기능을_강화시킨_컬렉션;
 
-public class Fruit {
+// 검색기능을 강화시킨 컬렉션
+// Comparable과 Comparator
+// Comparable을 구현하지 않은 클래스
 
+public class Fruit {
+	public String name;
+	public int price;
+	
+	public Fruit(String name, int price) {
+		this.name = name;
+		this.price = price;
+	}
 }
