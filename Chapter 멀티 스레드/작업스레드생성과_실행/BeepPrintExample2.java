555d1f1daa5c3dc6bbf3f63d07cc3a9c8ad2157f
jinusong <sugwer7464@gmail.com>
2018-07-20 23:57:52 +0900

    Thread 클래스부터 직접 생성 - 메인 스레드와 작업 스레드가 동신에 실행

--- Chapter 멀티 스레드/작업스레드생성과_실행/BeepPrintExample2.java
+++ Chapter 멀티 스레드/작업스레드생성과_실행/BeepPrintExample2.java
@@ -1,5 +1,9 @@
 package 작업스레드생성과_실행;
 
+// 작업스레드생성과 실생
+// Thread 클래스부터 직접 생성
+// 메인 스레드와 작업 스레드가 동신에 실행
+
 public class BeepPrintExample2 {
 	public static void main(String[] args) {	//메인 스레드
 		Runnable beepTask = new BeepTask();
