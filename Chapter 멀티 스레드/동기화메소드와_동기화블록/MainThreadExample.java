727bc0e60d436c991a6aadd6fc2aa0d8153b5455
jinusong <sugwer7464@gmail.com>
2018-07-20 23:56:04 +0900

    공유객체를 사용할 때의 주의할 점 - 메인 스레드가 실행하는 코드

--- Chapter 멀티 스레드/동기화메소드와_동기화블록/MainThreadExample.java
+++ Chapter 멀티 스레드/동기화메소드와_동기화블록/MainThreadExample.java
@@ -1,5 +1,9 @@
 package 동기화메소드와_동기화블록;
 
+// 동기화메소드와 동기화 블록
+// 공유객체를 사용할 때의 주의할 점
+// 메인 스레드가 실행하는 코드
+
 public class MainThreadExample {
 	public static void main(String[] args) {
 		Calculator calculator = new Calculator();
