66e57793927cbbf404af7e570ea207810bae6cd4
jinusong <sugwer7464@gmail.com>
2018-07-20 23:58:04 +0900

    Thread 하위 클래스로부터 생성 - 메인 스레드와 작업 스레드가 동시에 실행

--- Chapter 멀티 스레드/작업스레드생성과_실행/BeepPrintExample3.java
+++ Chapter 멀티 스레드/작업스레드생성과_실행/BeepPrintExample3.java
@@ -1,5 +1,9 @@
 package 작업스레드생성과_실행;
 
+// 작업스레드생성과 실생
+// Thread 하위 클래스로부터 생성
+// 메인 스레드와 작업 스레드가 동시에 실행
+
 public class BeepPrintExample3 {
 	public static void main(String[] args) {	//메인 스레드
 		Thread thread = new BeepThread();
