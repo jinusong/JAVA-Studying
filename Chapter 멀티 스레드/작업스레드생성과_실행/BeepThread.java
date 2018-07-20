e95c6110dafe14cfc9818dbb98d394152bdd16ae
jinusong <sugwer7464@gmail.com>
2018-07-20 23:58:31 +0900

    Thread 하위 클래스로부터 생성 - 비프음을 들려주는 스레드

--- Chapter 멀티 스레드/작업스레드생성과_실행/BeepThread.java
+++ Chapter 멀티 스레드/작업스레드생성과_실행/BeepThread.java
@@ -2,6 +2,10 @@ package 작업스레드생성과_실행;
 
 import java.awt.Toolkit;
 
+// 작업스레드생성과 실생
+// Thread 하위 클래스로부터 생성
+// 비프음을 들려주는 스레드
+
 public class BeepThread extends Thread{
 	@Override
 	public void run() {
