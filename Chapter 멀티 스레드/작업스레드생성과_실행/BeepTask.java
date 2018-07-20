ee6a474a3decd16e26977645e95dd5de2a644956
jinusong <sugwer7464@gmail.com>
2018-07-20 23:58:19 +0900

    Thread 클래스부터 직접 생성 - 비프음을 들려주는 작업 정의

--- Chapter 멀티 스레드/작업스레드생성과_실행/BeepTask.java
+++ Chapter 멀티 스레드/작업스레드생성과_실행/BeepTask.java
@@ -2,6 +2,10 @@ package 작업스레드생성과_실행;
 
 import java.awt.Toolkit;
 
+// 작업스레드생성과 실생
+// Thread 클래스부터 직접 생성
+// 비프음을 들려주는 작업 정의
+
 public class BeepTask implements Runnable{
 	public void run() {
 		Toolkit toolkit = Toolkit.getDefaultToolkit();	//스레드 실행 내용
